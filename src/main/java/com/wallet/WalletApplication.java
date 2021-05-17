d3d51569fe1a9975bb8f0343cd42caafc7831460
Fabio Vilela <rhcp_fabio@hotmail.com>
2021-05-17 13:08:24 -0300

    Configuração inicial para os testes

--- src/main/java/com/wallet/WalletApplication.java
+++ src/main/java/com/wallet/WalletApplication.java
@@ -3,6 +3,7 @@ package com.wallet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class WalletApplication {
 
--- src/test/java/com/wallet/HelloWorld.java
+++ src/test/java/com/wallet/HelloWorld.java
@@ -1,18 +1,21 @@
 package com.wallet;
 
-import org.junit.jupiter.api.Test;
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-@ActiveProfiles("test")
-class WalletApplicationTests {
-
+public class HelloWorld {
+	
 	@Test
-	void contextLoads() {
+	public void testHelloWorld() {
+		assertEquals(1,1);
+		
 	}
 
+	
 }
